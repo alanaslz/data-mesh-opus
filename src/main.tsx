943120028756ba75b81d3473abfaf8df247ec728
Alana Oliveira <alanaslz@gmail.com>
2025-07-23 09:40:05 -0300

    Corrige (1) para GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 👈 importar o BrowserRouter
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 importar BrowserRouter
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/data-mesh-opus/">
